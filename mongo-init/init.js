9019662645170e88f906f976dd8c773dbc116874
DrMack97 <delgadoantolinezdavidalejandro@gmail.com>
2026-05-18 17:33:29 +0000

    commit

---
+++ mongo-init/init.js
@@ -0,0 +1,10 @@
+{
+nom; String, // Nom del producte
+preu; Number, // Preu en euros (decimal)
+categoria; String, // 'electrònica', 'roba', 'llar', 'esport'...
+estoc; Number, // Unitats disponibles
+valoracio; Number, // De 1.0 a 5.0
+actiu; Boolean, // Si el producte està disponible per a vendre
+etiquetes; [String], // Array d'etiquetes
+creat_el; Date // Data de creació
+}
